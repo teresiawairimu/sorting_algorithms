448037b5a8dcc40ee4b31f697fd423660baeec54
teresiawairimu <teresiawairimu06@gmail.com>
2024-04-30 17:13:36 -0700

    task 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * @list: pointer to pointer list
+ * Return: list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *new = NULL;
+	listint_t *current = *list;
+	listint_t *temp;
+
+	while (current != NULL)
+	{
+		listint_t *next = current->next;
+		if (new == NULL || new->n >= current->n)
+		{
+			current->next = new;
+			current->prev = NULL;
+			if (new != NULL)
+			{
+				new->prev = current;
+			}
+			new = current;
+		}
+		else
+		{
+			*temp = new;
+			while (temp->next != NULL && temp->next->n < current->n)
+			{
+				temp = temp->next;
+			}
+			current->next = temp->next;
+			current->prev = temp;
+			if (temp->next != NULL)
+			{
+				temp->next->prev = current;
+			}
+			temp->next = current;
+		}
+		print_list(new);
+		current = next;
+	}
+	*list = new;
+}
+
